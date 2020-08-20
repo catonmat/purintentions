ca36e9ad18995d2de0e68f053689a643e50bb1fb
richard jarram <rjarram@me.com>
2020-08-20 10:09:01 -0400

    edited application.rb to disable sprockets; moved webpacker to root and renamed as frontend.

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,14 @@ module Purintentions
     # the framework and any gems in your application.
 
     # Don't generate system test files.
-    config.generators.system_tests = nil
+    # config.generators.system_tests = nil
+    config.generators do |g|
+      # Don't generate assets for Sprockets
+      g.assets = nil
+    
+      # Don't generate tests and helpers
+      g.test_framework = nil
+      g.helper = nil
+    end
   end
 end
