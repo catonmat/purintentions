5b7089de5b1154997b25ce57fbb02a07c21c98e8
richard jarram <rjarram@me.com>
2020-09-01 18:39:54 -0400

    cable scaffolded

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = request.session.fetch('username', nil)
+      reject_unathorized_connection unless current_user
+    end
   end
 end
+
---
+++ app/channels/chat_channel.rb
@@ -0,0 +1,24 @@
+class ChatChannel < ApplicationCable::Channel
+  # this method is called when a user is subscribed to the channel
+  def subscribed
+    # this method hooks the client's browser to a channel so it receives ws:// updates
+    stream_from "chat"
+  end
+
+  def send_message(payload)
+    message = Message.new(author: current_user, text: payload[:message])
+
+    ActionCable.server.broadcast("chat", message: render(message)) if message.save
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  private
+
+  def render(message)
+    # We create a new instance of ApplicationController to access the helper methods; we cannot access standard rails controllers from app/channels
+    ApplicationController.new.helpers.c("message", message: message)
+  end
+end
